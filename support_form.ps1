5693a80e1ae17d41006ed89cbd49c57fe509d591
jessetn <19999918+jessetn@users.noreply.github.com>
2019-04-12 08:10:52 -0500

    Update support_form.ps1

--- support_form.ps1
+++ support_form.ps1
@@ -172,7 +172,7 @@ $labelDisclaimer = New-Object System.Windows.Forms.Label
 $labelDisclaimer.Location = New-Object System.Drawing.Point(10,280)
 $labelDisclaimer.Size = New-Object System.Drawing.Size(420,40)
 $labelDisclaimer.Text = "$screenshotDisclaimer"
-$labelEmail.Font = [System.Drawing.Font]::new("Roboto", 10, [System.Drawing.FontStyle]::Bold)
+$labelDisclaimer.Font = [System.Drawing.Font]::new("Roboto", 10, [System.Drawing.FontStyle]::Bold)
 $form.Controls.Add($labelDisclaimer)
 
 ########################  Input Fields  ########################
